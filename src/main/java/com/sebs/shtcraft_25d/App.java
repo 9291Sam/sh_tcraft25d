474e9fd9191ed77b1a73723e51ea735e84b5d853
sebs macOS <sam.skinr@gmail.com>
2024-02-18 14:31:05 -0500

    test commit

--- src/main/java/com/sebs/shtcraft_25d/App.java
+++ src/main/java/com/sebs/shtcraft_25d/App.java
@@ -2,6 +2,6 @@ package com.sebs.shtcraft_25d;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello World! git commit test ");
   }
 }
