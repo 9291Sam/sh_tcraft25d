598ae353b0d13b2a108607bae46dbc371a6eba96
milan <milan@172.25.238.228>
2024-02-18 14:43:15 -0500

    Added a sysout

--- src/main/java/com/sebs/shtcraft_25d/App.java
+++ src/main/java/com/sebs/shtcraft_25d/App.java
@@ -3,5 +3,6 @@ package com.sebs.shtcraft_25d;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World! git commit test ");
+    System.out.println("Boo");
   }
 }
