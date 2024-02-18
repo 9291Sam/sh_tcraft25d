302b670325530ecf545433d2c1d8f89844880625
sebs macOS <sam.skinr@gmail.com>
2024-02-18 14:14:58 -0500

    inital commit

---
+++ src/main/java/com/sebs/shtcraft_25d/App.java
@@ -0,0 +1,7 @@
+package com.sebs.shtcraft_25d;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/sebs/shtcraft_25d/AppTest.java
@@ -0,0 +1,13 @@
+package com.sebs.shtcraft_25d;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
