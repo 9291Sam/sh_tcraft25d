9e114bb0958226068b182549f2edf21a3b4645ee
sebs macOS <sam.skinr@gmail.com>
2024-02-18 16:47:29 -0500

    sync

--- src/main/java/com/sebs/shtcraft_25d/Renderer.java
+++ src/main/java/com/sebs/shtcraft_25d/Renderer.java
@@ -3,7 +3,6 @@ package com.sebs.shtcraft_25d;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Optional;
 import java.util.Set;
 
 
@@ -11,7 +10,7 @@ import javax.swing.JPanel;
 
 public class Renderer extends JPanel implements KeyListener
 {
-	private Set<Entity> entities;
+	private Set<Renderable> entities;
 
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -33,9 +32,8 @@ public class Renderer extends JPanel implements KeyListener
 
 }
 
-interface Entity
+interface Renderable
 {
 	/// @return nullable
 	public Point getPosition();
-	///asdfs
 }
