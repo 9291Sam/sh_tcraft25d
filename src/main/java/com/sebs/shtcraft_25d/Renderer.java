943ad706f4ce59d9455bd0ce966ad84c3ad55b11
sebs macOS <sam.skinr@gmail.com>
2024-02-24 20:38:55 -0500

    Sync | weak Renderable things

--- src/main/java/com/sebs/shtcraft_25d/Renderer.java
+++ src/main/java/com/sebs/shtcraft_25d/Renderer.java
@@ -1,8 +1,12 @@
 package com.sebs.shtcraft_25d;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.lang.ref.WeakReference;
+import java.util.HashSet;
 import java.util.Set;
 
 
@@ -10,25 +14,50 @@ import javax.swing.JPanel;
 
 public class Renderer extends JPanel implements KeyListener
 {
-	private Set<Renderable> entities;
+	private Set<WeakReference<Renderable>> entities;
+	
+	public Renderer()
+	{
+		setPreferredSize(new Dimension(400, 400));
+		setBackground(Color.WHITE);
+		setFocusable(true);
+		addKeyListener(this);
+		
+		this.entities = new HashSet<>();
+		
+	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {
+	public void keyTyped(KeyEvent e)
+	{
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
+	public void keyPressed(KeyEvent e)
+	{
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void keyReleased(KeyEvent e) {
+	public void keyReleased(KeyEvent e)
+	{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private static void force_gc() {
+	    Object obj = new Object();
+	    WeakReference ref = new WeakReference<Object>(obj);
+	    obj = null;
+	    
+        while(ref.get() != null)
+        {
+	       System.gc();
+        }
+    }
 
 }
 
