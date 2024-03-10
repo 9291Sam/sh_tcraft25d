fc23ca3ac332e40db77e552d5e4aa77fdd026f38
sebs macOS <sam.skinr@gmail.com>
2024-03-10 14:52:34 -0400

    Repeating stone texture

--- src/main/java/com/sebs/shtcraft_25d/WorldManager.java
+++ src/main/java/com/sebs/shtcraft_25d/WorldManager.java
@@ -1,11 +1,33 @@
 package com.sebs.shtcraft_25d;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import com.sebs.shtcraft_25d.Renderer.DrawCallCollector;
 
 public class WorldManager implements Renderer.Entity
 {
+	Renderer renderer;
 	int widthTiles = 64;
 	int heightTiles = 64;
+	
+	Tile[][] tileMap = new Tile[this.widthTiles][this.heightTiles];
+	
+	private Image stone;
+	
+	public WorldManager(Renderer renderer_)
+	{
+		this.renderer = renderer_;	
+		
+		try {
+			this.stone = ImageIO.read(new File("stone.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	public void tick(double deltaTime) {}
@@ -13,12 +35,20 @@ public class WorldManager implements Renderer.Entity
 	@Override
 	public void draw(DrawCallCollector d) 
 	{
-		// TODO Auto-generated method stub
+		for (int i = -10; i < 10; ++i)
+		{
+			for (int j = -10; j < 10; ++j)
+			{
+				d.drawTexturedRectangle((double)i, (double)j, 1.0, 1.0, this.stone);
+			}
+		}
+//		double cameraX = this.renderer.getCameraXWorld();
+//		double cameraY = this.renderer.getCameraYWorld();
+//		
+//		int left = clamp(-this.widthTiles / 2.0, this.widthTiles / 2, cameraX - )
+		
 		
 	}
-	
-	
-	
 	
 	private enum Tile
 	{
@@ -26,11 +56,7 @@ public class WorldManager implements Renderer.Entity
 		Grass,
 	}
 
-
-
-
-
-
-	
-
+	public static double clamp(double min, double max, double val) {
+	    return Math.max(min, Math.min(max, val));
+	}
 }
