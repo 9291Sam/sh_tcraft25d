7228193ec7759d54351431dad1380c5ca5f06608
Jacob Devereaux <deverejt@miamioh.edu>
2024-04-24 13:43:27 -0400

    Merge branch 'main' of https://github.com/9291Sam/sh_tcraft25d.git into main

--- src/main/java/com/sebs/shtcraft_25d/UIManager.java
+++ src/main/java/com/sebs/shtcraft_25d/UIManager.java
@@ -7,10 +7,12 @@ import com.sebs.shtcraft_25d.Renderer.DrawCallCollector;
 public class UIManager implements Renderer.Entity
 {
     private Renderer renderer;
+    private double hp;
     
     public UIManager(Renderer renderer)
     {
-        this.renderer = renderer;   
+        this.renderer = renderer;
+        hp = 1.0;
     }
     
     @Override
@@ -20,8 +22,16 @@ public class UIManager implements Renderer.Entity
     public void draw(DrawCallCollector d)
     {
     	// TODO: do dynamic things
-    	d.drawFilledRectangleScreen(0.0, 0.95, 0, 0.25, 0.1, Color.GRAY);
-    	d.drawFilledRectangleScreen(0.025, 0.9675, 1, 0.2, 0.025, Color.RED);
+    	d.drawFilledRectangleScreen(0.0, 0.95, 20, 0.25, 0.1, Color.GRAY);
+    	d.drawFilledRectangleScreen(0.01, 0.96, 21, (0.23 * hp), 0.035, Color.RED);
+    }
+    
+    public double hit() {
+        return hp-= 0.25;
+    }
+    
+    public double regen() {
+         return hp += 0.001;
     }
 
 }
