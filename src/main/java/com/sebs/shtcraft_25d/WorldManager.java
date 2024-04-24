908e1fea96621996e5d279959367a92cd477346e
Jacob Devereaux <deverejt@miamioh.edu>
2024-04-24 13:15:44 -0400

    WorldManage upd

--- src/main/java/com/sebs/shtcraft_25d/WorldManager.java
+++ src/main/java/com/sebs/shtcraft_25d/WorldManager.java
@@ -1,6 +1,9 @@
 package com.sebs.shtcraft_25d;
 
+
 import java.awt.Image;
+import java.util.Random;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -10,47 +13,108 @@ import com.sebs.shtcraft_25d.Renderer.DrawCallCollector;
 
 public class WorldManager implements Renderer.Entity
 {
-	Renderer renderer;
-	int widthTiles = 64;
-	int heightTiles = 64;
-	
-	Tile[][] tileMap = new Tile[this.widthTiles][this.heightTiles];
-	
-	private Image stone;
-	
-	public WorldManager(Renderer renderer_)
-	{
-		this.renderer = renderer_;	
-		
-		try {
-			this.stone = ImageIO.read(new File("stone.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Override
-	public void tick(double deltaTime) {}
+    Renderer renderer;
+    int widthTiles = 64;
+    int heightTiles = 64;
+    int centerX;
+    int centerY;
+    int width;
+    int height;
+    private Rectangle[] rectangles = new Rectangle[50];
+    
+    Tile[][] tileMap = new Tile[this.widthTiles][this.heightTiles];
+    
+    private Image stone;
+    private Image grass;
+    private Random random = new Random();
+    private int modX;
+    private int modY;
+    
+    public static Image loadImage(String filePath) 
+    {
+        File file = new File(filePath);
+        try {
+            return ImageIO.read(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return(null);
+        }
+    }
+    
+    public WorldManager(Renderer renderer_)
+    {
+        modX = 0;
+        modY = 0;
+        this.renderer = renderer_;  
+        
+        this.stone = loadImage("stone.png");
 
-	@Override
-	public void draw(DrawCallCollector d) 
-	{
-		for (int i = -10; i < 10; ++i)
-		{
-			for (int j = -10; j < 10; ++j)
-			{
-				d.drawTexturedRectangleWorld((double)i, (double)j, -1, 1.0, 1.0, this.stone);
-			}
-		}
-	}
-	
-	private enum Tile
-	{
-		Stone,
-		Grass,
-	}
+        this.grass = loadImage("grassTexure.png");
+        
+        for(int i = 0; i < rectangles.length; i++) {
+            centerX = modX;
+            centerY = modY;
+            width = randomGen();
+            height = randomGen();
+            rectangles[i] = new Rectangle(centerX, centerY, width, height);
+            modX = random.nextInt(83) - 43;
+            modY = random.nextInt(83) - 43;
+        }
+    }
+    
+    @Override
+    public void tick(double deltaTime) {}
 
-	public static double clamp(double min, double max, double val) {
-	    return Math.max(min, Math.min(max, val));
-	}
+    @Override
+    public void draw(DrawCallCollector d) 
+    {
+        for (int i = -50; i < 50; ++i)
+        {
+            for (int j = -50; j < 50; ++j)
+            {
+                d.drawTexturedRectangleWorld((double)i, (double)j, -1, 1.0, 1.0, this.stone);
+            }
+        }
+        
+        for (Rectangle rect : rectangles) {
+            drawRect(d, rect.centerX, rect.centerY, rect.width, rect.height);
+        }
+    }
+    
+    private int randomGen() {
+        int rand = random.nextInt(7) + 10;
+        return rand;
+    }
+    
+    private void drawRect(DrawCallCollector d, int centerX, int centerY, int width, int height) {
+        for (int i = centerX - width / 2; i < centerX + width / 2; ++i) {
+            for (int k = centerY - height / 2; k < centerY + height / 2; ++k ) {
+                d.drawTexturedRectangleWorld((double)i + 1, (double)k, 0, 1.0, 1.0, this.grass);
+            }
+        }
+    }
+    
+    private enum Tile
+    {
+        Stone,
+        Grass,
+    }
+
+    public static double clamp(double min, double max, double val) {
+        return Math.max(min, Math.min(max, val));
+    }
+    
+    private class Rectangle{ 
+        int centerX;
+        int centerY;
+        int width;
+        int height;
+        
+        public Rectangle(int x, int y, int w, int h) {
+            centerX = x;
+            centerY = y;
+            width = w;
+            height = h;
+        }
+    }
 }
